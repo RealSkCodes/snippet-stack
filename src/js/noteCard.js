f7e10d661ee5435954d7a391465e16f1b35798b6
RealSk <codingwithrealsk@gmail.com>
2024-10-15 11:55:48 +0530

    Make noteCard function into component and export it

--- src/js/noteCard.js
+++ src/js/noteCard.js
@@ -1,6 +1,6 @@
 const notesContainer = document.getElementById("notes-container")
 
-const noteCard = (id, image, date, title, description) => {
+export const noteCard = (id, image, date, title, description) => {
   // create card div to connect to notes container
   const cardDiv = document.createElement("div")
   notesContainer.append(cardDiv)
@@ -46,39 +46,3 @@ const noteCard = (id, image, date, title, description) => {
     })
   })
 }
-
-noteCard(
-  "5",
-  "https://icons.iconarchive.com/icons/ampeross/qetto-2/256/photos-icon.png",
-  "15/10/2075",
-  "testtitle",
-  "testdesc"
-)
-noteCard(
-  "4",
-  "https://w0.peakpx.com/wallpaper/730/501/HD-wallpaper-iphone-14-pro-thumbnail.jpg",
-  "14/10/2075",
-  "testtitle testtitletesttitle testtitle testtitle",
-  "test description test description test description test description test description test description test description test description"
-)
-noteCard(
-  "3",
-  "https://icons.iconarchive.com/icons/ampeross/qetto-2/256/photos-icon.png",
-  "13/10/2075",
-  "testtitle",
-  "testdesc"
-)
-noteCard(
-  "2",
-  "https://icons.iconarchive.com/icons/ampeross/qetto-2/256/photos-icon.png",
-  "12/10/2075",
-  "testtitle",
-  "testdesc"
-)
-noteCard(
-  "1",
-  "https://icons.iconarchive.com/icons/ampeross/qetto-2/256/photos-icon.png",
-  "11/10/2075",
-  "testtitle",
-  "testdesc"
-)
