dc046b8f1c0a9313bfd7d12d3ab4279d187c84a2
RealSk <codingwithrealsk@gmail.com>
2024-10-19 11:12:54 +0530

    Create basic get notes and add notes function

--- src/controller.js
+++ src/controller.js
@@ -7,8 +7,19 @@ const getNotes = (req, res) => {
     res.status(200).json(results.rows)
   })
 }
-const controller = {
-  getNotes,
+
+const addNotes = (req, res) => {
+  const { image_url, category, title, description } = req.body
+  pool.query(queries.addNotesQuery, [image_url, category, title, description], (error, results) => {
+    if (error) throw error
+    res.status(201).send("Note Added Successfully!")
+    console.log("Note Added Successfully!")
+  })
 }
 
+const controller = {
+  getNotes,
+  addNotes,
+}
+// might make it one line later
 export default controller
