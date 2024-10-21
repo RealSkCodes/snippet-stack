933a9f45a2051b6c31b228bbc7f1f7ab33ff70f8
RealSk <codingwithrealsk@gmail.com>
2024-10-21 09:26:29 +0530

    Add fucntion to fetch the data from sql db to frontend

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,20 @@
 import { createNote } from "./createNote.js"
+import { noteCard } from "./noteCard.js"
+
+const loadNotes = async () => {
+  const fetchResult = await fetch("http://localhost:3000/api/v1/notes")
+  const result = await fetchResult.json()
+  const notesContainer = document.getElementById("notes-container")
+  notesContainer.innerHTML = ""
+  result.forEach((element) => {
+    noteCard(element.id, element.image_url, element.category, element.title, element.description)
+  })
+}
 
 const runAll = async () => {
-  const categories = ["Work", "Personal", "Ideas"] // Example categories
-  await createNote(categories)
+  await loadNotes()
+  const categories = ["Work", "Personal", "Ideas"]
+  await createNote(categories, loadNotes)
 }
+
 runAll()
