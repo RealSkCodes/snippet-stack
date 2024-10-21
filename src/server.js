f8a8710ec8bdfd90e0270f626a879ff57fa8aa46
RealSk <codingwithrealsk@gmail.com>
2024-10-21 10:21:02 +0530

    Modify server to server index.html file

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
-import express from "express" // Correctly import express as a default import
+import express from "express"
 import cors from "cors"
+import path from "path"
 import noteRoutes from "./routes.js"
+
 const app = express()
 const port = 3000
 
+// Middleware to enable CORS
 app.use(cors())
 
+// Middleware to parse JSON bodies
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello world")
+// Serve static files from the "public" folder
+app.use("/src", express.static("src"))
+
+// Serve the index.html file for any route except API routes
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.resolve("public", "index.html"))
 })
 
+// API routes for handling notes
 app.use("/api/v1/notes", noteRoutes)
 
-app.listen(port, () => console.log(`App listening on port: ${port}`))
+// Start the server
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`)
+})
