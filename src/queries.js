130632a181ee1c061b168367ea0787c5da68cbf4
RealSk <codingwithrealsk@gmail.com>
2024-10-21 10:20:16 +0530

    Add getNotesByCategory query

--- src/queries.js
+++ src/queries.js
@@ -1,6 +1,7 @@
 const getNotesQuery = "SELECT * FROM notes"
 const addNotesQuery =
   "INSERT INTO notes (image_url, category, title, description) VALUES ($1, $2, $3, $4)"
+const getNotesByCategoryQuery = `SELECT * FROM notes WHERE category = $1`
 
-const queries = { getNotesQuery, addNotesQuery }
+const queries = { getNotesQuery, addNotesQuery, getNotesByCategoryQuery }
 export default queries
