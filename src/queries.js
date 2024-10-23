4f3b265896b14dab32d54f1e682335c660b64e6e
RealSk <codingwithrealsk@gmail.com>
2024-10-23 09:21:46 +0530

    Add getCategoriesQuery

--- src/queries.js
+++ src/queries.js
@@ -1,7 +1,8 @@
-const getNotesQuery = "SELECT * FROM notes"
+const getNotesQuery = "SELECT * FROM notes;"
 const addNotesQuery =
-  "INSERT INTO notes (image_url, category, title, description) VALUES ($1, $2, $3, $4)"
-const getNotesByCategoryQuery = `SELECT * FROM notes WHERE category = $1`
+  "INSERT INTO notes (image_url, category, title, description) VALUES ($1, $2, $3, $4);"
+const getNotesByCategoryQuery = `SELECT * FROM notes WHERE category = $1;`
+const getCategoriesQuery = `SELECT DISTINCT notes.category FROM notes;`
 
-const queries = { getNotesQuery, addNotesQuery, getNotesByCategoryQuery }
+const queries = { getNotesQuery, addNotesQuery, getNotesByCategoryQuery, getCategoriesQuery }
 export default queries
