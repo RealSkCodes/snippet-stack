6d548093ee98fce5e90f57e32c9ff7ffcec78728
RealSk <codingwithrealsk@gmail.com>
2024-10-23 12:41:24 +0530

    Add deleteCardDataQuery

--- src/queries.js
+++ src/queries.js
@@ -3,6 +3,13 @@ const addNotesQuery =
   "INSERT INTO notes (image_url, category, title, description) VALUES ($1, $2, $3, $4);"
 const getNotesByCategoryQuery = `SELECT * FROM notes WHERE category = $1;`
 const getCategoriesQuery = `SELECT DISTINCT notes.category FROM notes;`
+const deleteCardDataQuery = "DELETE FROM notes WHERE id = $1;"
 
-const queries = { getNotesQuery, addNotesQuery, getNotesByCategoryQuery, getCategoriesQuery }
+const queries = {
+  getNotesQuery,
+  addNotesQuery,
+  getNotesByCategoryQuery,
+  getCategoriesQuery,
+  deleteCardDataQuery,
+}
 export default queries
