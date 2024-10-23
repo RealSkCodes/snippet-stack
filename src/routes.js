fed351c5d5315de36e145fc540f0bc64da063ce3
RealSk <codingwithrealsk@gmail.com>
2024-10-23 09:26:11 +0530

    Add /categories route

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,9 @@ import controller from "./controller.js"
 
 const router = Router()
 
+router.get("/categories", controller.getCategories)
+router.get("/:category", controller.getNotesByCategory)
 router.get("/", controller.getNotes)
 router.post("/", controller.addNotes)
-router.get("/:category", controller.getNotesByCategory)
 
 export default router
