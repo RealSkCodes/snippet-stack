d76fbe339f7d712985e1136c439c2f6002062a14
RealSk <codingwithrealsk@gmail.com>
2024-10-23 12:41:46 +0530

    Add deleteCardData route

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ router.get("/categories", controller.getCategories)
 router.get("/:category", controller.getNotesByCategory)
 router.get("/", controller.getNotes)
 router.post("/", controller.addNotes)
+router.delete("/:id", controller.deleteCardData) // Assuming deleteCardData is in your controller
 
 export default router
